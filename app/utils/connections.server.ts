b605d3398c328d9a5e3761c96114c16912476942
Yaswanth reddy Desam <yaswanth@Yaswanths-MacBook-Pro-2.local>
2024-11-22 18:04:26 +0530

    remove litefs proxy server config

--- app/utils/connections.server.ts
+++ app/utils/connections.server.ts
@@ -1,6 +1,6 @@
 import { createCookieSessionStorage } from '@remix-run/node'
 import { type ProviderName } from './connections.tsx'
-import { GitHubProvider } from './providers/github.server.ts'
+// import { GitHubProvider } from './providers/github.server.ts'
 import { type AuthProvider } from './providers/provider.ts'
 import { type Timings } from './timing.server.ts'
 
@@ -17,7 +17,7 @@ export const connectionSessionStorage = createCookieSessionStorage({
 })
 
 export const providers: Record<ProviderName, AuthProvider> = {
-	github: new GitHubProvider(),
+	// github: new GitHubProvider(),
 }
 
 export function handleMockAction(providerName: ProviderName, request: Request) {
